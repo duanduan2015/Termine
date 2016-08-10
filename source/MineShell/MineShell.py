4b78aae3f99d15fea5150733de2b6cbd78761aad
duanduan <yunduan.han@gmail.com>
2016-08-10 00:18:42 -0700

    add manual deploy mines and test every shell unit

--- source/MineShell/MineShell.py
+++ source/MineShell/MineShell.py
@@ -5,6 +5,8 @@ class MineShell:
     def __init__(self):
         self.field = None
         self.space = ' '
+        self.flagNum = 0
+        self.mineNum = 0
         #self.deploy = None
 
     def getInput(self, string):
@@ -19,7 +21,75 @@ class MineShell:
             return self.peek(int(strings[1]), int(strings[2]))
         if (strings[0] == 'flag'):
             return self.flag(int(strings[1]), int(strings[2]))
+        if (strings[0] == 'unflag'):
+            return self.unflag(int(strings[1]), int(strings[2]))
+        if strings[0] == 'deploy':
+            return self.deploy(int(strings[1]), int(strings[2]))
+        if (strings[0] == 'query'):
+            if strings[1] == 'flags':
+                return str(self.flagNum)
+            if strings[1] == 'mines':
+                return str(self.mineNum)
+            if strings[1] == 'width':
+                return str(self.field.width)
+            if strings[1] == 'height':
+                return str(self.field.height)
+            if strings[1] == 'success':
+                if self.isSuccess():
+                    return 'yes'
+                else:
+                    return 'no'
+    def deploy(self, x, y):
+        if x < 0 or x >= self.field.width or y < 0 or y >= self.field.height:
+            return 'out of bounds'
+        self.field.status[y][x] = -1
+        self.updateStatusAround(x, y)
+        return str(x) + self.space + str(y) + self.space + 'deployed'
+    def updateStatusAround(self, x, y):
+        height = self.field.height
+        width = self.field.width
+        if y - 1 >= 0:
+            self.field.status[y - 1][x] += 1
+        if y + 1 < height:
+            self.field.status[y + 1][x] += 1
+        if x - 1 >= 0:
+            self.field.status[y][x - 1] += 1
+        if x + 1 < width:
+            self.field.status[y][x + 1] += 1
+        if y - 1 >= 0 and x - 1 >= 0:
+            self.field.status[y - 1][x - 1] += 1
+        if y + 1 < height and x + 1 < width:
+            self.field.status[y + 1][x + 1] += 1
+        if y - 1 >= 0 and x + 1 > width:
+            self.field.status[y - 1][x + 1] += 1
+        if y + 1 < height and x - 1 >= 0:
+            self.field.status[y + 1][x - 1] += 1
         
+
+
+    def isSuccess(self):
+        if self.flagNum != self.mineNum:
+            return False
+        for y in range(self.field.height):
+            for x in range(self.field.width):
+                if self.field.status[y][x] == -1 and self.field.flagged[y][x] == False:
+                    return False
+        return True
+
+    def flag(self, x, y):
+        if x < 0 or x >= self.field.width or y < 0 or y >= self.field.height:
+            return 'out of bounds'
+        if self.field.flagged[y][x] is False:
+            self.field.flagged[y][x] = True
+            self.flagNum = self.flagNum + 1
+        return str(x) + self.space + str(y) + self.space + 'flagged'
+
+    def unflag(self, x, y):
+        if x < 0 or x >= self.field.width or y < 0 or y >= self.field.height:
+            return 'out of bounds'
+        if (self.field.flagged[y][x] == True):
+            self.field.flagged[y][x] = False
+            self.flagNum = self.flagNum - 1
     
     
     def peek(self, x, y):
@@ -35,6 +105,7 @@ class MineShell:
 
     def createMineField(self, height, width, numOfMines):
         self.field = MineField(height, width) 
+        self.mineNum = numOfMines
         #self.deploy = Deploy(numOfMines, sefl.field)
         return 'created minefield ' + str(20) + ' x ' + str(10) + ' with ' + str(30)+' mines'
 
@@ -43,7 +114,7 @@ class MineShell:
             return 'no minefield'
         if (x < 0 or x >= self.field.width or y < 0 or y >= self.field.width):
             return 'out of bounds'
-        status = self.field.status
+        status = self.field.status[y][x]
         if (status == -1) :
             return 'boom'
         else:
@@ -57,48 +128,48 @@ class MineShell:
                 py = p[1]
                 if px - 1 >= 0:
                     if py - 1 >= 0:
-                        if not (px - 1, py - 1) in pos:
+                        if not (px - 1, py - 1) in pos and self.field.status[py - 1][px - 1] != -1:
                             pos.add((px - 1, py - 1))
                             self.field.opened[py - 1][px - 1] = True
                             if self.field.status[py - 1][px - 1] == 0:
                                 queue.put((px - 1, py - 1))
                     if py + 1 < self.field.height:
-                        if not (px - 1, py + 1) in pos:
+                        if not (px - 1, py + 1) in pos and self.field.status[py + 1][px - 1] != -1:
                             pos.add((px - 1, py + 1))
                             self.field.opened[py + 1][px - 1] = True
                             if self.field.status[py + 1][px - 1] == 0:
                                 queue.put((px - 1, py + 1))
-                    if not (px - 1, py) in pos:
+                    if not (px - 1, py) in pos and self.field.status[py][px - 1] != -1:
                         pos.add((px - 1, py))
                         self.field.opened[py][px - 1] = True
                         if self.field.status[py][px - 1] == 0:
                             queue.put((px - 1, py))
                 if py - 1 >= 0:
-                    if not (px, py - 1) in pos:
+                    if not (px, py - 1) in pos and self.field.status[py - 1][px] != -1:
                         pos.add((px, py - 1))
                         self.field.opened[py - 1][px] = True
                         if self.field.status[py - 1][px] == 0:
                             queue.put((px, py - 1))
                 if py + 1 < self.field.height:
-                    if not (px, py + 1) in pos:
+                    if not (px, py + 1) in pos and self.field.status[py + 1][px] != -1:
                         pos.add((px, py + 1))
                         self.field.opened[py + 1][px] = True
                         if self.field.status[py + 1][px] == 0:
                             queue.put((px, py + 1))
                 if px + 1 < self.field.width:
                     if py - 1 >= 0:
-                        if not (px + 1, py - 1) in pos:
+                        if not (px + 1, py - 1) in pos and self.field.status[py - 1][px + 1] != -1:
                             pos.add((px + 1, py - 1))
                             self.field.opened[py - 1][px + 1] = True
                             if self.field.status[py - 1][px + 1] == 0:
                                 queue.put((px + 1, py - 1))
                     if py + 1 < self.field.height:
-                        if not (px + 1, py + 1) in pos:
+                        if not (px + 1, py + 1) in pos and self.field.status[py + 1][px + 1] != -1:
                             pos.add((px + 1, py + 1))
                             self.field.opened[py + 1][px + 1] = True
                             if self.field.status[py + 1][px + 1] == 0:
                                 queue.put((px + 1, py + 1))
-                    if not (px + 1, py) in pos:
+                    if not (px + 1, py) in pos and self.field.status[py][px + 1] != -1:
                         pos.add((px + 1, py))
                         self.field.opened[py][px + 1] = True
                         if self.field.status[py][px + 1] == 0:
